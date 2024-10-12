b9463f4d97dc8a4a3a6ced23c63b89b5d41887ed
Luis Martins <luisfilipemartins2003@gmail.com>
2024-10-12 18:40:21 +0100

    gg

---
+++ src/main/java/model/Library.java
@@ -0,0 +1,25 @@
+package model;
+import java.util.UUID;
+
+public class Library {
+    private UUID id;
+    private String name;
+    private String address;
+    private String openTime;
+    private String closeTime;
+    private String openDays;
+    private String openStatement;
+    private boolean open;
+
+    public Library(UUID id, String name, String address, String openTime, String closeTime, String openDays, String openStatement, boolean open) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.openTime = openTime;
+        this.closeTime = closeTime;
+        this.openDays = openDays;
+        this.openStatement = openStatement;
+        this.open = open;
+    }
+}
+
